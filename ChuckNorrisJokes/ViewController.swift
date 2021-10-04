35ec4cec2106c12ea2c6e1dfb9717aa7b7564d9b
vladislav <vlad.shtytcko@lookout.com>
2021-10-04 14:39:28 +0300

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Vladislav on 4.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
